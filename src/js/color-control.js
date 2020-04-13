12d20b9b6cce2c9990ba61ba48a7cb34cf718bae
Dima Chuchukalo <Dimachuchukalo@gmail.com>
2020-04-13 14:21:03 +0300

    ready project

---
+++ src/js/color-control.js
@@ -0,0 +1,31 @@
+import colors from './arr-colors';
+import refs from './refs';
+import randomIntegerFromInterval from './random';
+
+const startBtn = refs.startButtonRef;
+const stopBtn = refs.stopButtonRef;
+const bodyBtn = refs.bodyRef;
+
+let timerId = null;
+
+const setColor = () => {
+  timerId = setInterval(() => {
+    const colorNumber = randomIntegerFromInterval(0, 5);
+    bodyBtn.setAttribute('style', `background-color: ${colors[colorNumber]};`);
+  }, 1000);
+  startBtn.setAttribute('disabled', 'true');
+  startBtn.removeEventListener('click', setColor);
+  stopBtn.removeAttribute('disabled');
+};
+
+const stopSetCollor = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  startBtn.addEventListener('click', setColor);
+  bodyBtn.removeAttribute('style');
+  stopBtn.setAttribute('disabled', 'true');
+};
+
+stopBtn.setAttribute('disabled', 'true');
+startBtn.addEventListener('click', setColor);
+stopBtn.addEventListener('click', stopSetCollor);
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  startButtonRef: document.querySelector('[data-action="start"]'),
+  stopButtonRef: document.querySelector('[data-action="stop"]'),
+  bodyRef: document.querySelector('body'),
+};
